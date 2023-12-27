bd7fa26bcbd18e2a1b42a8959d428758c43e376e
Devansh Gupta <devansh.gupta@jungleegames.com>
2023-12-28 02:44:38 +0530

    added: support for tokeization

--- lib/Tokenizor.hs
+++ lib/Tokenizor.hs
@@ -1,36 +1,49 @@
-module Tokenizor(parser,Token) where
+module Tokenizor(Token(..),tokenParser) where
 
-import Text.Parsec.Char (char, oneOf,endOfLine,anyChar,string,digit,spaces,space) 
-import Text.Parsec (parsecMap,manyTill,try,choice,eof,many1,(<|>))
+import Text.Parsec.Char (char, oneOf,endOfLine,anyChar,string,digit,spaces,space,letter,octDigit, hexDigit)
+import Text.Parsec (manyTill,try,choice,many1,(<|>), many,option,eof,count, unexpected  )
 import Text.Parsec.String (Parser)
+import Data.Int (Int32)
+import Numeric (readHex,readOct,readBin, readFloat)
 
-data Token = Plus |  Minus
+data Token = Plus  |  Minus
  | Mul | Divide | CommentHash | Assign | Eq
- | Not | NotEq | LTH | GTH | LTEQ | GTEQ | Mod | SemiColon | LeftParen | RightParen | WhiteSpace
- | LeftShift | RightShift | Comment  
- | NumInt Integer | NumFloat Double deriving (Show)
+ | Not | NotEq | LTH | GTH | LTEQ | GTEQ | Mod | SemiColon | LeftParen | RightParen
+ | LeftBrace | RightBrace |  WhiteSpace
+ | Comma | Colon | FullStop
+ | NewAssign
+ | BitwiseAND | BitwiseOR
+ | BitwiseXOR
+ | CondAND | CondOR
+ | Break | Case | Const | Continue | Default | Else | For | Func | Goto | If
+ | Import | Map | Package | Range | Return | Struct | Switch | Type | Var
+ | LeftShift | RightShift | Comment | Identifier String
+ | RuneLit Int32
+ | StringLit [Int32]
+ | IntLit Integer | FloatLit Double deriving (Show,Eq)
 
 
 
-parseUnitTokens :: Parser  Token 
-parseUnitTokens = do
-    c <- oneOf "+-*%;/()"  
-    return $ case c of
-        '+' -> Plus
-        '-' -> Minus
-        '*' -> Mul
-        '%' -> Mod
-        ';' -> SemiColon
-        '/' -> Divide
-        '(' -> LeftParen
-        ')' -> RightParen
-    
 
-
-parseBinaryTokens :: Parser Token
-parseBinaryTokens = do
-    result <- choice ar 
+-- operator parses an operator
+operator :: Parser Token
+operator = do
+    result <- choice ar
     return $ case result of
+        "." -> FullStop
+        "," -> Comma
+        ":=" -> NewAssign
+        ":" -> Colon
+        "+" -> Plus
+        "-" -> Minus
+        "*" -> Mul
+        "%" -> Mod
+        ";" -> SemiColon
+        "/" -> Divide
+        "(" -> LeftParen
+        ")" -> RightParen
+        "{" -> LeftBrace
+        "}" -> RightBrace
         "<<" -> LeftShift
         "<=" -> LTEQ
         "<" -> LTH
@@ -40,46 +53,280 @@ parseBinaryTokens = do
         "==" -> Eq
         "=" -> Assign
         "!=" -> NotEq
-        "!" -> Not 
-    where ar = fmap (try.string)  ["<<","<=","<",">>",">=",">","==","=","!=","!"] 
+        "!" -> Not
+        "&&" -> CondAND
+        "&" -> BitwiseAND
+        "||" -> CondOR
+        "|" -> BitwiseOR
+        "^" -> BitwiseXOR
+    where ar = fmap (try.string)  [".",",",":=",":","+","-","*","%",";","/","(",")","{","}","<<","<=","<",">>",">=",">","==","=","!=","!","&&","&","||","|","^"]
     -- we are using try because string combinator does backtrace in case of failure
 
 
-parseFloat :: Parser Token
-parseFloat = do
-    number <- many1 digit 
-    char '.'
-    floatingPart <- many1 digit
-    return $ NumFloat (read (number ++ "." ++ floatingPart) :: Double)
+
+binaryDigit_ :: Parser Char
+binaryDigit_ = oneOf "01"
+
+
+letter_ :: Parser Char
+letter_ = letter <|> char '_'
+
+-- identifierOrkeyword parses an identifier or keyword
+identifierOrkeyword :: Parser Token
+identifierOrkeyword = do
+    prefix <- letter
+    sufix <- many suffixParser
+    return $ let
+        name = (prefix:sufix)
+        in case name of
+            "break" -> Break
+            "case" -> Case
+            "const" -> Const
+            "continue" -> Continue
+            "default" -> Default
+            "else" -> Else
+            "for" -> For
+            "func" -> Func
+            "goto" -> Goto
+            "if" -> If
+            "import" -> Import
+            "map" -> Map
+            "package" -> Package
+            "range" -> Range
+            "return" -> Return
+            "struct" -> Struct
+            "switch" -> Switch
+            "type" -> Type
+            "var" -> Var
+            _ ->  Identifier name
+    where suffixParser = letter_ <|> digit
+
+
+integer :: Parser Token
+integer = let
+
+
+    bin_lit_ = do
+        digits <-many1 binaryDigit_
+        return $ IntLit $ retrive $ readBin digits
+
+    hex_lit_ = do
+        digits <- many1 hexDigit
+        return $ IntLit $ retrive $ readHex digits
+
+    oct_lit_ = do
+        digits <- many1 octDigit
+        return $ IntLit $ retrive $ readOct digits
+
+    num = do
+        d <- digit
+        if d == '0' then do 
+            -- following literal can be octal, binary or hexadecimal
+            -- lets try to look for octal digits without prefix 'o' or 'O'
+            oct_digits <- many octDigit
+            if not (null oct_digits) then return $ IntLit $ retrive $ readOct oct_digits
+            else do
+                -- check for hexa, binary and octal literal
+                -- we are using option parser as there might be nothing to parse ahead
+                -- and 0 is the value
+                f <- option '0' $ oneOf "bBoOxX"
+                non_decimal_literal f 
+        else do
+            suf <-  many digit
+            return $ IntLit $ read (d:suf)
     
-parseInteger :: Parser Token    
-parseInteger = do
-    number <- many1 digit
-    return $ NumInt ( read number :: Integer)
+    non_decimal_literal l = case l of
 
-parseNumber :: Parser Token 
-parseNumber = try parseFloat <|> parseInteger 
+        'b' -> bin_lit_
+        'B' -> bin_lit_
+        'x' -> hex_lit_
+        'X' -> hex_lit_
+        'o' -> oct_lit_
+        'O' -> oct_lit_
+        _ -> return $ IntLit 0
+
+    retrive [(x,_)] = x
+
+    in num
+
+-- parser for floating point number
+-- while parsing numbers we should give this method priority over 
+-- integer and operator parser 
+-- we are considering operator parser because a 
+-- literal starting from "." can be a full stop also
+-- and we are checking that possibility in float parser as well
+float :: Parser Token
+float = let
+
+    retrive [(x,_)] = x
+
+    decimal_exponent_ = do
+        x <- oneOf "eE"
+        y <-option '+' $ oneOf "+-"
+        z<-many1 digit
+        return (x:y:z)
+
+    no_intregal_ = do
+        digits <- many digit
+        if not (null digits) then do
+            expo <- option [] decimal_exponent_
+
+            return $  FloatLit $ retrive $ readFloat ("0." ++ digits ++ expo)
+        else
+            return $ FullStop
+    fractional_ = do
+        frac_digits <- many digit
+        expo <- option [] decimal_exponent_
+        return $ frac_digits ++ expo
+
+    with_intregal_ = do
+        digits <- many1 digit
+        x <- option '0' $ char '.'  
+        case x of
+                '.' -> do
+                        y <-  fractional_  
+                        return $ FloatLit $ retrive $ readFloat $ digits ++ "." ++ if not (null y) then y else "0"
+                _ -> do 
+                    y <- option [] decimal_exponent_   
+                    case y of
+                        [] -> unexpected "need to be parsed as integer" 
+                        _ -> return $ FloatLit $ retrive $ readFloat $ digits ++ y 
 
 
-parseComment :: Parser Token 
-parseComment = do
-    char '#' 
-    manyTill anyChar $ try endOfLine 
-    return Comment 
+    float_lit_ = do
+        x <- option '0' $ char '.'
+        case x of
+            -- literal without any intregal part, e.g. .256, .12345E+5
+            '.' -> no_intregal_
+            _ -> try with_intregal_ -- we have applied "try" as there is a possibility of having an integer 
 
-parseWhiteSpace :: Parser Token
-parseWhiteSpace = do
+    in float_lit_ 
+
+
+runeLit_ :: Parser Int32
+runeLit_ = let
+
+    retrive [(x,_)] = x 
+
+    rune_lit__ :: Parser Int32
+    rune_lit__ = do
+        _ <- char '\\'
+        y <- option '0' $ oneOf "abfnrtvuUx\\'\""
+        case escaped_char y of
+            0 -> 
+                case y of  
+                    'U' -> big_u_val 
+                    'u' -> little_u_val 
+                    'x' -> hex_byte_val 
+                    _ -> oct_byte_val 
+            _ -> return $ escaped_char y
+
+    escaped_char :: Char -> Int32
+    escaped_char n = 
+            case n of
+                'a' -> 0x7
+                'b' -> 0x8
+                'f' -> 0xc
+                'n' -> 0xa
+                'r' -> 0xd
+                't' -> 0x9
+                'v' -> 0xb
+                '\\' -> 0x5c
+                '\'' -> 0x27
+                '\"' -> 0x22
+                _ -> 0
+
+
+    big_u_val = do
+        d <- count 8 hexDigit
+        return $ retrive $ readHex d 
+
+    little_u_val = do
+        d <- count 4 hexDigit
+        return $ retrive $ readHex d
+
+    hex_byte_val = do
+        d <- count 2 hexDigit
+        return $ retrive $ readHex d
+    oct_byte_val = do
+        d <- count 3 octDigit
+        return $ retrive $ readOct d
+
+    unicode_char :: Parser Int32
+    unicode_char = do
+        x <- anyChar
+        return ( fromIntegral (fromEnum x) :: Int32)
+    in rune_lit__ <|> unicode_char
+    
+rune :: Parser Token
+rune = do
+        _ <- char '\''
+        x <- runeLit_   
+        _ <- char '\''
+        return $ RuneLit x
+
+str :: Parser Token
+str = let
+    toInt32 n = fromIntegral (fromEnum n) :: Int32
+
+    raw_str :: Parser Token
+    raw_str = do
+        lit <- manyTill anyChar $ char '`' 
+        return $ StringLit $ fmap toInt32 lit 
+
+    interpreted_str :: Parser Token
+    interpreted_str = do
+        lit <- manyTill runeLit_ $ char '"'
+        return $ StringLit $ fmap toInt32 lit 
+
+    in do 
+        x <- oneOf "`\""
+        case x of
+            '`' ->  raw_str 
+            _ -> interpreted_str 
+
+
+
+-- comment parses comments
+-- comments started with // would end at the end of line
+-- comments started with /* would end at */ 
+comment :: Parser Token
+comment = do
+    _ <- char '/'
+    x <- oneOf "/*"
+    _ <- case x of
+            '/' -> manyTill anyChar endOfComment  
+            _ -> manyTill anyChar $ try $ string "*/"
+    return Comment
+    where endOfComment = do
+            x <- option '0' endOfLine
+            case x of
+                        '0' -> eof *> return '\n'
+                        _ -> return '\n'  
+-- whiteSpace parses white space
+whiteSpace :: Parser Token
+whiteSpace = do
     space
     spaces
     return WhiteSpace
 
-parser :: Parser [Token] 
-parser = let 
-    parsers = choice [
-        parseWhiteSpace,
-        parseUnitTokens,
-        parseComment,
-        parseBinaryTokens,
-        parseNumber
-        ] 
-    in manyTill parsers eof
+
+tokenParser :: Parser [Token]
+tokenParser = let
+    -- Note: Order of these parsers really matters
+    parser_combinator = choice [
+        whiteSpace,
+        comment,
+        rune,
+        str,
+        float,
+        integer,
+        identifierOrkeyword,
+        operator]
+    exculder x = case x of
+        Comment -> False
+        WhiteSpace -> False
+        _ -> True
+    in do 
+        tokens <- manyTill parser_combinator eof  
+        return $ filter exculder tokens 
--- test/Main.hs
+++ test/Main.hs
@@ -1,4 +1,75 @@
-module Main (main) where
--- Run the tests
-main :: IO ()
-main = putStrLn "Hello World" 
+module Main where
+
+import Text.Parsec (parse)
+import Test.HUnit
+import Tokenizor
+
+-- Helper function to parse tokens
+parseTokens :: String -> [Token]
+parseTokens input = case parse tokenParser "" input of
+    Left err -> error (show err)
+    Right tokens -> tokens
+
+-- Test cases for the token parser
+testTokenParser :: Test
+testTokenParser = test [
+    -- Basic tokens
+    "Integer literal" ~: "123" ~: [IntLit 123] ~=? parseTokens "123",
+    "Float literal" ~: "3.14" ~: [FloatLit 3.14] ~=? parseTokens "3.14",
+    "Identifier" ~: "foo" ~: [Identifier "foo"] ~=? parseTokens "foo",
+    "Operator" ~: "+" ~: [Plus] ~=? parseTokens "+",
+    "Comment" ~: "// This is a comment" ~: [] ~=? parseTokens "// This is a comment",
+    
+    -- Keywords
+    "Keywords" ~: "if else for func return" ~: [If, Else, For, Func, Return] ~=? parseTokens "if else for func return",
+    
+    -- Complex expressions
+    "Complex expression" ~: "x + 42 * y" ~: [Identifier "x", Plus, IntLit 42, Mul, Identifier "y"] ~=? parseTokens "x + 42 * y",
+    
+    -- Strings and runes
+    "String literal" ~: "\"Hello, World!\"" ~: [StringLit [72,101,108,108,111,44,32,87,111,114,108,100,33]] ~=? parseTokens "\"Hello, World!\"",
+    "Rune literal" ~: "'a'" ~: [RuneLit 97] ~=? parseTokens "'a'",
+    
+    -- Special characters and operators
+    "Special characters" ~: "{ ( ) }" ~: [LeftBrace, LeftParen, RightParen, RightBrace] ~=? parseTokens "{ ( ) }",
+    "Bitwise operators" ~: "& | ^" ~: [BitwiseAND, BitwiseOR,BitwiseXOR] ~=? parseTokens "& | ^",
+    
+    -- White space and comments
+    "White space" ~: "  x  " ~: [Identifier "x"] ~=? parseTokens "  x  ",
+    "Comments" ~: "/* This is a comment */" ~: [] ~=? parseTokens "/* This is a comment */",
+    
+    -- Edge cases
+    "Empty input" ~: "" ~: [] ~=? parseTokens "",
+    
+    -- Test different literals
+    "Binary integer literal" ~: "0b101" ~: [IntLit 5] ~=? parseTokens "0b101",
+    "Hexadecimal integer literal" ~: "0x1A" ~: [IntLit 26] ~=? parseTokens "0x1A",
+    "Octal integer literal" ~: "0o777" ~: [IntLit 511] ~=? parseTokens "0o777",
+    
+    -- Test floating-point literals
+    "Floating-point literal" ~: "1.23" ~: [FloatLit 1.23] ~=? parseTokens "1.23",
+    "Exponential notation" ~: "1.2e3" ~: [FloatLit 1200.0] ~=? parseTokens "1.2e3",
+    
+    -- Test string literals
+    "Interpreted string literal" ~: "\"Hello\"" ~: [StringLit [72, 101, 108, 108, 111]] ~=? parseTokens "\"Hello\"",
+    "Raw string literal" ~: "`Hello`" ~: [StringLit [72, 101, 108, 108, 111]] ~=? parseTokens "`Hello`",
+    
+    -- Test rune literals
+    "Escape characters in rune" ~: "'\\n'" ~: [RuneLit 10] ~=? parseTokens "'\\n'",
+    "Unicode character in rune" ~: "'\x03A9'" ~: [RuneLit 937] ~=? parseTokens "'\x03A9'",
+    
+    -- Test for specific operators
+    "Left shift operator" ~: "<<" ~: [LeftShift] ~=? parseTokens "<<",
+    "Right shift operator" ~: ">>" ~: [RightShift] ~=? parseTokens ">>",
+    "Not equal operator" ~: "!=" ~: [NotEq] ~=? parseTokens "!=",
+    "Less than or equal operator" ~: "<=" ~: [LTEQ] ~=? parseTokens "<=",
+    "Greater than or equal operator" ~: ">=" ~: [GTEQ] ~=? parseTokens ">=",
+    "Short variable declaration" ~: ":=" ~: [NewAssign] ~=? parseTokens ":=",
+    
+    -- Test for white space and comments within expressions
+    "Whitespace within expression" ~: "x  +  y" ~: [Identifier "x", Plus, Identifier "y"] ~=? parseTokens "x  +  y",
+    "Comment within expression" ~: "x /* comment */ + y" ~: [Identifier "x", Plus, Identifier "y"] ~=? parseTokens "x /* comment */ + y"]
+
+-- Run the test suite
+main :: IO Counts
+main = runTestTT testTokenParser
